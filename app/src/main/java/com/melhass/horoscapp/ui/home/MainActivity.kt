c90708c84eef69dc0a6431788fb63a2d830657e6
Melina Hassan <melina.hassan@alumnos.frgp.utn.edu.ar>
2023-11-14 15:03:13 -0300

    creacion

---
+++ app/src/main/java/com/melhass/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.melhass.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.melhass.horoscapp.R
+import com.melhass.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController:NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        iniciarNavegacion()
+    }
+
+    private fun iniciarNavegacion() {
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fcvMain) as NavHostFragment
+        navController = navHost.navController
+        binding.bnvMenu.setupWithNavController(navController)
+    }
+}
